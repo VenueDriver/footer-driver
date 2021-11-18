e0f0fb4aff9e86a6d38b71c9d48edf0b2b2f3380
Derek Norrbom <dnorrbom@hakkasan.com>
2021-11-18 15:03:10 -0800

    deployment
    dev and staging

--- deployment/bin/footer-driver.ts
+++ deployment/bin/footer-driver.ts
@@ -1,7 +1,7 @@
 import { App, SecretValue } from '@aws-cdk/core';
 import { FooterDriverPipelineStack } from '../lib/footer-driver-pipeline-stack';
-//import { FooterDriverPipelineStackDevelopment } from '../lib/footer-driver-pipeline-stack-development';
-//import { FooterDriverPipelineStackStaging } from '../lib/footer-driver-pipeline-stack-staging';
+import { FooterDriverPipelineStackDevelopment } from '../lib/footer-driver-pipeline-stack-development';
+import { FooterDriverPipelineStackStaging } from '../lib/footer-driver-pipeline-stack-staging';
 
 const app = new App();
 
@@ -14,15 +14,15 @@ new FooterDriverPipelineStack(app, 'footer-driver-pipeline-production', {
     account: footerDriverDeployAccount, region: 'us-east-1' },
 });
 
-// new FooterDriverPipelineStackStaging(app, 'footer-driver-pipeline-staging', {
-//   env: { 
-//     account: footerDriverDeployAccount, region: 'us-east-1' },
-// });
+new FooterDriverPipelineStackStaging(app, 'footer-driver-pipeline-staging', {
+  env: { 
+    account: footerDriverDeployAccount, region: 'us-east-1' },
+});
 
-// new FooterDriverPipelineStackDevelopment(app, 'footer-driver-pipeline-development', {
-//   env: { 
-//     account: footerDriverDeployAccount, region: 'us-east-1' },
-// });
+new FooterDriverPipelineStackDevelopment(app, 'footer-driver-pipeline-development', {
+  env: { 
+    account: footerDriverDeployAccount, region: 'us-east-1' },
+});
 
 app.synth();
 
